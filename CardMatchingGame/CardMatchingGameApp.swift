88e16faf211afcfae1d46fff1b61ca9c19df7645
Jalal Hemidach <outawa@gmail.com>
2025-12-08 18:55:49 -0500

    Project creation + few simple changes for the canvas preview

---
+++ CardMatchingGame/CardMatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardMatchingGameApp.swift
+//  CardMatchingGame
+//
+//  Created by Jalal Hemidach on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CardMatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardMatchingGameTests/CardMatchingGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardMatchingGameTests.swift
+//  CardMatchingGameTests
+//
+//  Created by Jalal Hemidach on 12/8/25.
+//
+
+import Testing
+@testable import CardMatchingGame
+
+struct CardMatchingGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
